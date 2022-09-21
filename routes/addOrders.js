051522d495fa5ed7f1d2e9623a27fef0fea553c3
beskar <baavimw@gmail.com>
2022-09-22 01:38:10 +1000

    API to add Order to DB

---
+++ routes/addOrders.js
@@ -0,0 +1,27 @@
+import express from "express";
+import { db } from "../config/db.js";
+
+const router = express.Router();
+router.post("/addorder", async (_, res) => {
+  try {
+    let transaction_id = req.body.transaction_id;
+    let member_id = req.body.member_id;
+    const sqlQuery =
+      "INSERT INTO Transaction (transaction_id, member_id) VALUES ('transaction_id', 'member_id');";
+    db.query(sqlQuery, function (err, result) {
+      if (err) throw err;
+      db.end();
+
+      return res.status(200).json({
+        status: 200,
+      });
+    });
+  } catch (error) {
+    return res.status(400).json({
+      status: 400,
+      // data: error.message,
+      data: "An error occured in the backend",
+    });
+  }
+});
+export default router;
